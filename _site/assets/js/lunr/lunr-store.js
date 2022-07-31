8d147af13bb61a5615a06a96949b14cbcda86318
cinescope <cinescope@kaist.ac.kr>
2022-07-31 11:04:25 +0900

    update
    update

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "[Jekyll] 시험용 포스트",
+        "excerpt":"Jekyll을 통한 정적 웹사이트 생성 예시     Jekyll을 이용한 정적 웹사이트를 생성하는 과정을 보여준다.         시작     종료      로컬 서버 확인  ","categories": ["Blog"],
+        "tags": ["Blog","jekyll","Github","Git"],
+        "url": "/blog/the-first-post/",
+        "teaser": null
+      }]
