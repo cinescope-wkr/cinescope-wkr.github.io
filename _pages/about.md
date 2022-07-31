1beffc111d01ba7a6d9aece044cade8095397f9c
cinescope <cinescope@kaist.ac.kr>
2022-07-31 14:01:51 +0900

    revision on about.md

--- _pages/about.md
+++ _pages/about.md
@@ -6,10 +6,17 @@ layout: single
 
 ## About the blog 
 ---
-이 블로그는 시각예술과 기술의 결합을 다루고자 한다. 
-시각예술 분야의 공상을 기술을 통해 현실의 영역으로 데려오는 것에 관심을 가지고 있다. 
+**시각예술이 기술과 결합하며 탄생하는 가치**에 대해서 관심을 가지고 있다.  
+어떻게 시각예술 분야의 공상을 과학기술을 통해 실현할 수 있을지 생각을 나눈다.<br><br>
+블로그에서 다루는 구체적인 주제는 아래와 같다. 
+- computational imagining 
+- computational photography 
+- computer graphics
+- human-computer interation 
+- cinematogrphy 
+- personal documents 
 
 ## About me 
 ---
-KAIST Electrical Engineering and Computer Science (Double Major)
-KAIST RUN
+- KAIST Electrical Engineering and Computer Science (Double Major)<br>
+- KAIST RUN
--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -6,7 +6,7 @@ var store = [{
         "teaser": null
       },{
     "title": "About Futura, From",
-    "excerpt":"About the blog   이 블로그는 시각예술과 기술의 결합을 다루고자 한다.  시각예술 분야의 공상을 기술을 통해 현실의 영역으로 데려오는 것에 관심을 가지고 있다.   About me   KAIST Electrical Engineering and Computer Science (Double Major) KAIST RUN  ","url": "http://localhost:4000/about/"
+    "excerpt":"About the blog 시각예술이 기술과 결합하며 탄생하는 가치에 대해서 관심을 가지고 있다. 어떻게 시각예술 분야의 공상을 과학기술을 통해 실현할 수 있을지 생각을 나눈다. 블로그에서 다루는 구체적인 주제는 아래와 같다. computational imagining computational photography computer graphics human-computer interation cinematogrphy personal documents About me KAIST Electrical Engineering and Computer Science (Double Major)...","url": "http://localhost:4000/about/"
   },{
     "title": null,
     "excerpt":"","url": "http://localhost:4000/"
